5a73842423a84b9f2ce2240661b4aabc67b207cd
Your Name <you@example.com>
2019-12-11 15:58:58 +0800

    finish

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ok } from 'assert';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUser() {
+    return 'OK';
+  }
+  @Get(':id')
+  getOneUser(@Param('id') id) {
+    return 'OK';
+  }
+  @Post()
+  postUser(@Body() body) {
+    return 'OK';
+  }
+  @Put(':id')
+  putUser(@Body() body, @Param('id') id) {
+    return 'OK';
+  }
+  @Delete(':id')
+  deleteUser(@Param('id') id) {
+    return 'OK';
   }
 }
